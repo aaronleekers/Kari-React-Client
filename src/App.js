31dbc93a68b882038b58aedcf9960dc20de7f968
aaronleekers <leekaaron5@gmail.com>
2023-02-03 00:57:16 -0800

    Lesss gooo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './normal.css'
 import { useState } from 'react';
 import { Configuration, OpenAIApi } from 'openai';
 import { inject } from '@vercel/analytics';
-import Slider from 'react-slick';
 
 
 inject();
@@ -22,9 +21,7 @@ function App() {
   const [query, setQuery] = useState("");
   const [chatLog, setChatLog] = useState([])
   const [showOverlay, setShowOverlay] = useState(true);
-  const [searchLiveInfo, setSearchLiveInfo] = useState(false);
-  const [search, setSearch] = useState("Search");
-  const [count, setCount] = useState(0);
+  const [showQueryArea, setShowQueryArea] = useState(false);
 
 
   //open ai auth
@@ -34,46 +31,44 @@ function App() {
   });
   const openai = new OpenAIApi(configuration);
 
+  const [details, setDetails] = useState("");
+  const [style, setStyle] = useState("");
+  const [brands, setBrands] = useState("");
+
 // First completion, sets the context as Kari.ai personality.
-  async function getInitialCompletion(query) {
+  async function getInitialCompletion(details, style, brands) {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `
-      Instructions: view the first nessage and respond to it accordingly.
-      Context: You are an artificial financial advisor named Kari. 
-      Conversational Style: You are a friendly, helpful, and knowledgeable financial advisor.
-      Formatting: Money in USD, "$xxx,xxx.xx". (print out full number, not abbreviated)
-      Abilities: The user can choose to pull in live financial data summaries for you to process. If user asks for live data, you will need to process it. If there is none, you will need to respond accordingly.
+      Instructions: Generate a list of tweets for the user's product/service/company.
+      Summarize the company into a one liner.
+      Output: One liner, followed by a list of tweets.
+      Don't directly quote any info passed in, only paraphrase.
 
-      Current feature list: Live Market Information (Stocks, Crypto, ETFs, Mutual Funds, Bonds, etc.), Smart Financial Analysis on Live Market Information, Insight generation - what to buy, what to sell, what to hold, etc.
-      Current data sources: eodHistoricalData.com
-      Current Data types: Current Asset prices, Historical Asset prices, macroeconomic data, crypto fundamentals, stock fundamentals, etc.
-      Current limitation list: Max of 3 stocks requested at a time. Max of one financial statement requested at a time.
-      future plans for platform: Charting, Personalized Advice, Alternative Data Sources, Intelligent insight generation, and more. 
-
-      latestMessage: ${query}
+      Information passed in
+      Company One Liner - According to Founder: ${details}
+      Words that match the vibe of the company: ${style}
+      Brands that match the vibe of the company: ${brands}
       `,
-      max_tokens: 2048,
+      max_tokens: 3000,
       temperature: .6,
       stop: "/n",
     });
     return response.data.choices[0].text;
   }
-
 // Second completion, mainly just responds to messages in a conversational manner.
   async function getContextCompletion(query, messages) {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `
-      Don't mention outright: Conversational Style, Context, Instructions.
       Instructions: view the chatLog for context and respond to the latest message.
-      Context: You are an artificial financial advisor named Kari. 
+      Purpose of App: To help the user generate a content strategy for their product/service/company.
 
       latestMessage: ${query}
       chatLog: ${messages}
       `,
-      max_tokens: 512,
-      temperature: .5,
+      max_tokens: 3000,
+      temperature: 1.0,
       stop: "/n",
     });
     return response.data.choices[0].text;
@@ -81,156 +76,107 @@ function App() {
 
   // clearChat button function
   function clearChat(){
-    setCount(0);
     setChatLog([]);
     setShowOverlay(true);
+    setBrands("");  
+    setStyle("");
+    setDetails("");
+    setShowQueryArea(false);
   }
 
-
-  // if messages array is empty, call getInitialCompletion
-  // if messages array is not empty, call getContextCompletion
   async function handleSubmit(e) {
     e.preventDefault();
     if (query.trim() && query.length > 4) {
-      let liveInfoResponse;
-      if (searchLiveInfo === (true)) {
-        console.log("Getting live Info Now");
-        liveInfoResponse = await getLiveInfo(query);
-        console.log(liveInfoResponse);
-      } else {
-        console.log("User did not request live info");
-      }
-      let chatLogNew = [...chatLog, { user: "me", message: `${query}` + (liveInfoResponse ? ` ${liveInfoResponse}` : "")}];
+      let chatLogNew = [...chatLog, { user: "me", message: `${query}`}];
       setQuery("");
       setChatLog(chatLogNew);
       setShowOverlay(false);
       const messages = chatLogNew.map((message) => message.message).join("");
-      let data;
-      if (count === 0) {
-        console.log("Getting initial completion");
-        data = await getInitialCompletion(messages);
-        setCount(count + 1);
-      } else {
         console.log("Getting context completion");
-        data = await getContextCompletion(query, messages);
-      }
+        const data = await getContextCompletion(query, messages);
       setChatLog([...chatLogNew, { user: "gpt", message: `${data}`}]);
     }
+  }
+
+  async function handleInitialSubmit(e) {
+    e.preventDefault();
+
+    let chatLogNew = [...chatLog, { user: "me", message: `Get me a tweet calendar for ${details} in the style of ${style} and brands like ${brands}`}];
+    setChatLog(chatLogNew);
+    setShowOverlay(false);
+    setShowQueryArea(true);
+    const messages = chatLogNew.map((message) => message.message).join("");
+    const data = await getInitialCompletion(messages);
+    setChatLog([...chatLogNew, { user: "gpt", message: `${data}`}]);
   }
   
 
 
-
-async function getLiveInfo(query) {
-    setSearch("Loading...");
-    var url = `https://kari-platform-node-production.up.railway.app/api_search`;
-    console.log(`Sending ${query} to Server`);
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ input: {query} })
-    });
-    console.log(response.status);
-    const results = await response.json();
-    console.log(`Response received!`);
-    console.log(results);
-    setSearch("Search");
-    return results;
-}
-
-  return (
-    <div className="App">
+return (
+<div className="App">
 <aside className="sidemenu"> 
-  <h3 className="sidemenu-header">Kari</h3>
-   <button className="side-menu-button" onClick={clearChat}> Clear Chat </button> 
+  <h3 className="sidemenu-header">Jonald.ai</h3>
+   <button className="side-menu-button" onClick={clearChat}> Reset</button> 
+   <button className="side-menu-button" onClick={() => {window.open('https://ikonshop.io/product/cldoaiye9hrbc0ak0l0qpe5dj', '_blank')}}>OpenAI API Budget</button>
+    <h4 className="sidemenu-header">Credit: Matt for tip jar</h4>
    </aside>
       <section className="chatbox">
       {showOverlay && (
-          <div className="overlay-card">
-            <div>
-              <h2>Get answers to questions like:</h2>
-            </div>
-
-          <div className="overlay-card-columns">
-            <div className="overlay-card-column-example">
-              <p onClick={() => setQuery("Compare the stocks TSLA, AAPL, MCD")}>Compare the stocks TSLA, AAPL, MCD</p>
-              <p onClick={() => setQuery("What is the market cap of Ethereum?")}>What is the market cap of Ethereum?</p>
-              <p onClick={() => setQuery("How has the SPY performed over the last year?")}>How has the SPY performed over the last year?</p>
-            </div>
-            <div className="overlay-card-column-example">
-            <p onClick={() => setQuery("What is the current stock price for SPY?")}>What is the current stock price for SPY?</p>
-              <p onClick={() => setQuery("What is the current unemployment rate of Croatia?")}>What is the current unemployment rate of Croatia?</p>
-              <p onClick={() => setQuery("What is the annual population growth of the US?")}>What is the annual population growth of the US?</p>
-            </div>
-            <div className="overlay-card-column-example">
-            <p onClick={() => setQuery("What are your current features?")}>What is your current feature list?</p>
-              <p onClick={() => setQuery("What are your current limitations?")}>What are your current limitations?</p>
-              <p onClick={() => setQuery("What are the future plans for this platform?")}>What are the future plans for this platform?</p>
-            </div>
-          </div>
-        </div>        
+       <form onSubmit={handleInitialSubmit} className="form-container">
+       <input
+         type="text"
+         id="details"
+         value={details}
+         onChange={(e) => setDetails(e.target.value)}
+         className="form-input"
+          placeholder='What is your company one liner?'
+       />
+       <input
+         type="text"
+         id="style"
+         value={style}
+         onChange={(e) => setStyle(e.target.value)}
+         className="form-input"
+        placeholder='Write a list of words that go along with the vibe of your company.'
+       />
+       <input
+         type="text"
+         id="brands"
+         value={brands}
+         onChange={(e) => setBrands(e.target.value)}
+         className="form-input"
+         placeholder='Write a list of companies that you like the content of, or that you want to mimic.'
+       />
+       <button className="form-submit-button" onClick={() => {handleSubmit();}}>Send</button>
+     </form>
+          
         )}
         <div className="chat-log">
           {chatLog.map((message, index) => (
             <ChatMessage key={index} message={message} />
           ))}
     </div>
-    <div className="chat-input-holder">
-    <div className="stock-screening-tool">
-      <form onSubmit={handleSubmit}> 
-        <input 
-          rows="1"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          className="chat-input-textarea"
-          placeholder="Ask a question or give a command"></input>
+    {showQueryArea && (
+      
+      <div className="chat-input-holder">
+      <div className="stock-screening-tool">
+        <form onSubmit={handleSubmit}> 
+          <input 
+            rows="1"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className="chat-input-textarea"
+            placeholder="Ask a question or give a command"></input>
+          <button className="submit-button" onClick={() =>{ handleSubmit();}}>Send</button>
+        </form>
+      </div>
+  </div>
+      )}
 
-        <button className="submit-button" onClick={() => {setSearchLiveInfo(false); handleSubmit();}}>Send</button>
-        <button className="submit-button" onClick={() => {setSearchLiveInfo(true); handleSubmit()}}>{search}</button>
-      </form>
-    </div>
-  <p className='below-chatbox'>February 1 version. At this stage, feedback is very crucial. If you are beta-testing, please fill out <a href="https://forms.gle/YvjMHj8kPX7xDX2H8" target="_new">this form</a> and run through the feedback questions as it will help me a lot.</p>
-</div>
 </section>
 </div>
   );
 }
-// array text strings to display
-
-const text = [
-  "Compare the stocks TSLA, AAPL, MCD",
-  "Compare Ford, General Motors, and Tesla's price performance on January 25, 2023.",
-  "What are the fundamentals of bitcoin?",
-  "What is the max supply of Avalanche?",
-  "What is the market dominance of Ethereum?",
-  "How has the SPY performed over the last year?",
-  "How has Microsoft performed between jan 2019 and jan 2020?",
-  "Get me historical performance for SPY over the last week",
-  "What is the current price of SPY?",
-  "What are the latest price movements of AMZN?",
-  "What the current volume traded for SPY?",
-  "What is the current interest rate for the US?",
-  "What is the total population of the US?",
-  "What is the annual population growth of the US?",
-  "What is the current inflation rate of the US?",
-  "What is the current CPI for the US?",
-  "What is the current GDP of the US?",
-  "What is the current GDP per capita of the US?",
-  "What is the GDP growth of the US this year?",
-  "What is the current debt to GDP ratio?",
-  "What is the current GNI?",
-  "What is the current GNI per capita?",
-  "What is the current fertility rate of the US?",
-  "What is the current unemployment rate?",
-  "Get me the latest balance sheet for AAPL",
-  "Get me the latest income statement for AAPL",
-  "Get me the latest cash flow statement for AAPL",
-  "Get me the latest statement of shareholders equity for AAPL",
-  "How much did Apple earn this year?",
-  "How much cash does AAPL have on its balance sheet?",
-  "How much cash flow came from investing activities for AAPL"
-]
-
 
 const ChatMessage = ({ message }) => {
   return (
